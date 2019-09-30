1c0ecab5c243f802822f9e590db602488c978bd4
alnhdgrlla <y.tanaka@shippio.io>
2019-09-30 16:00:24 +0900

    added edit user info page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,27 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   before_action :authenticate_user!
 
-  has_many :tweets, depedent: :destroy
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+private
+
+def user_params
+  params.require(:user).permit(:username, :password, :password_confirmation, :bio)
+end
+
 end
