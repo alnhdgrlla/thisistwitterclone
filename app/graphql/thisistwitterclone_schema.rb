5d2421fb26d35321afd1efe1f8cb7eb14c7bd16e
alnhdgrlla <y.tanaka@shippio.io>
2019-10-04 10:35:51 +0900

    relationship model added

---
+++ app/graphql/thisistwitterclone_schema.rb
@@ -0,0 +1,4 @@
+class ThisistwittercloneSchema < GraphQL::Schema
+  mutation(Types::MutationType)
+  query(Types::QueryType)
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_085908) do
+ActiveRecord::Schema.define(version: 2019_10_01_050617) do
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followee_id"], name: "index_relationships_on_followee_id"
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
